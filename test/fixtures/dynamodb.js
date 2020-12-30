15cb484fa88aa49450edff6801c05968bd9f32ef
jdrydn <james@jdrydn.com>
2020-12-30 00:21:52 +0000

    Fixing tests

--- test/fixtures/dynamodb.js
+++ test/fixtures/dynamodb.js
@@ -10,12 +10,13 @@ const logger = createLogger();
 async function createModel(opts) {
   assert(_isPlainObject(opts), 'Expected createModel opts to be a plain object');
 
-  const { dynamodb, tableName, createTable, keySchema, properties, ...createOpts } = opts;
+  const { dynamodb, tableName, createTable, properties, keySchema, options, ...createOpts } = opts;
   assert(dynamodb instanceof AWS.DynamoDB, 'Expected createModel dynamodb to be an instance of AWS.DynamoDB');
   assert(tableName && typeof tableName === 'string', 'Expected createModel opts.tableName to be a string');
   assert(!createTable || _isPlainObject(createTable), 'Expected createModel opts.createTable to be a plain object');
-  assert(!keySchema || _isPlainObject(keySchema), 'Expected createModel opts.keySchema to be a plain object');
   assert(!properties || _isPlainObject(properties), 'Expected createModel opts.properties to be a plain object');
+  assert(!keySchema || _isPlainObject(keySchema), 'Expected createModel opts.keySchema to be a plain object');
+  assert(!options || _isPlainObject(options), 'Expected createModel opts.options to be a plain object');
 
   await deleteThenCreateTable(dynamodb, {
     TableName: tableName,
@@ -42,6 +43,11 @@ async function createModel(opts) {
         default: () => uuid(),
       },
       ...properties,
+    },
+    options: {
+      createdAtTimestamp: true,
+      updatedAtTimestamp: true,
+      ...options,
     },
     ...createOpts,
   });
