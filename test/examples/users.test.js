cace099b5a722fd0b3a196009e0ec664f9a11dc0
jdrydn <james@jdrydn.com>
2021-01-02 20:58:30 +0000

    Cleaned up users test & added bulk tests

--- test/examples/users.test.js
+++ test/examples/users.test.js
@@ -1,13 +1,15 @@
 const _ = require('lodash');
 const assert = require('assert');
 const mockdate = require('mockdate');
+const rewire = require('rewire');
 const { dynamodb, assertItem, deleteThenCreateTable } = require('../utils');
-const { users, createTable } = require('../../examples/users');
 
 describe('examples', () => describe('users', () => {
+  const users = rewire('../../examples/users');
   const currentDate = new Date();
 
   before(async () => {
+    const createTable = users.__get__('createTable');
     await deleteThenCreateTable(dynamodb, createTable);
     mockdate.set(currentDate);
   });
@@ -16,13 +18,39 @@ describe('examples', () => describe('users', () => {
     mockdate.reset();
   });
 
+  it('should fail to create an entry without required fields', async () => {
+    try {
+      await users.create({});
+      assert.fail('Should have thrown an error');
+    } catch (err) {
+      assert.ok(err instanceof Error);
+      assert.strictEqual(err.message, 'Expected all required fields to be set');
+      assert.deepStrictEqual(err.fields, [ 'email', 'name' ]);
+    }
+  });
+
+  it('should fail to create an entry with an invalid enum value', async () => {
+    try {
+      await users.create({
+        email: 'jdrydn@github.io',
+        name: 'James',
+        role: 'HACKER',
+      });
+      assert.fail('Should have thrown an error');
+    } catch (err) {
+      assert.ok(err instanceof Error);
+      assert.strictEqual(err.message, '[dynazord-example-users] [role]: Expected value to be one of: ADMIN, MODERATOR, EDITOR, USER');
+      assert.strictEqual(err.value, 'HACKER');
+    }
+  });
+
   it('should create a new entry', async () => {
-    const user = await users.create({
+    const entry = await users.create({
       email: 'jdrydn@github.io',
       name: 'James',
     });
 
-    assert.ok(_.isPlainObject(user), 'Expected users.create to return a plain object');
+    assert.ok(_.isPlainObject(entry), 'Expected users.create to return a plain object');
 
     await assertItem(dynamodb, {
       TableName: users.tableName,
@@ -35,7 +63,7 @@ describe('examples', () => describe('users', () => {
       updatedAt: { S: currentDate.toISOString() },
     });
 
-    assert.deepStrictEqual(user, {
+    assert.deepStrictEqual(entry, {
       email: 'jdrydn@github.io',
       name: 'James',
       role: 'USER',
@@ -45,10 +73,10 @@ describe('examples', () => describe('users', () => {
   });
 
   it('should get the entry', async () => {
-    const user = await users.get({ email: 'jdrydn@github.io' });
+    const entry = await users.get({ email: 'jdrydn@github.io' });
 
-    assert.ok(_.isPlainObject(user), 'Expected users.get to return a plain object');
-    assert.deepStrictEqual(user, {
+    assert.ok(_.isPlainObject(entry), 'Expected users.get to return a plain object');
+    assert.deepStrictEqual(entry, {
       email: 'jdrydn@github.io',
       name: 'James',
       role: 'USER',
@@ -58,14 +86,14 @@ describe('examples', () => describe('users', () => {
   });
 
   it('should update the entry', async () => {
-    const user = await users.update({
+    const entry = await users.update({
       avatarUrl: 'https://github.com/jdrydn.png',
       role: 'MODERATOR',
     }, {
       email: 'jdrydn@github.io',
     });
 
-    assert.ok(_.isPlainObject(user), 'Expected users.update to return a plain object');
+    assert.ok(_.isPlainObject(entry), 'Expected users.update to return a plain object');
 
     await assertItem(dynamodb, {
       TableName: users.tableName,
@@ -79,7 +107,7 @@ describe('examples', () => describe('users', () => {
       updatedAt: { S: currentDate.toISOString() },
     });
 
-    assert.deepStrictEqual(user, {
+    assert.deepStrictEqual(entry, {
       email: 'jdrydn@github.io',
       name: 'James',
       avatarUrl: 'https://github.com/jdrydn.png',
@@ -87,6 +115,22 @@ describe('examples', () => describe('users', () => {
       createdAt: currentDate,
       updatedAt: currentDate,
     });
+  });
+
+  it('should fail to update the entry\'s avatar URL', async () => {
+    try {
+      await users.update({
+        avatarUrl: 'data:application/vnd.microsoft.portable-executable,MEGAMAN.EXE',
+      }, {
+        email: 'jdrydn@github.io',
+      });
+      assert.fail('Should have thrown an error');
+    } catch (err) {
+      assert.ok(err instanceof Error);
+      assert.strictEqual(err.message, '[dynazord-example-users] [avatarUrl]: Expected value to be a base64-image or URL');
+      assert.strictEqual(err.status, 400);
+      assert.strictEqual(err.value, 'data:application/vnd.microsoft.portable-executable,MEGAMAN.EXE');
+    }
   });
 
   it('should delete the entry', async () => {
@@ -100,13 +144,13 @@ describe('examples', () => describe('users', () => {
   });
 
   it('should upsert the entry', async () => {
-    const user1 = await users.upsert({
+    const entry1 = await users.upsert({
       email: 'jdrydn@github.io',
       name: 'James',
       role: 'MODERATOR',
     });
 
-    assert.ok(_.isPlainObject(user1), 'Expected users.update to return a plain object');
+    assert.ok(_.isPlainObject(entry1), 'Expected users.update to return a plain object');
 
     await assertItem(dynamodb, {
       TableName: users.tableName,
@@ -119,7 +163,7 @@ describe('examples', () => describe('users', () => {
       updatedAt: { S: currentDate.toISOString() },
     });
 
-    assert.deepStrictEqual(user1, {
+    assert.deepStrictEqual(entry1, {
       email: 'jdrydn@github.io',
       name: 'James',
       role: 'MODERATOR',
@@ -127,14 +171,14 @@ describe('examples', () => describe('users', () => {
       updatedAt: currentDate,
     });
 
-    const user2 = await users.upsert({
+    const entry2 = await users.upsert({
       email: 'jdrydn@github.io',
       name: 'James',
       role: 'MODERATOR',
       avatarUrl: 'https://github.com/jdrydn.png',
     });
 
-    assert.ok(_.isPlainObject(user2), 'Expected users.update to return a plain object');
+    assert.ok(_.isPlainObject(entry2), 'Expected users.update to return a plain object');
 
     await assertItem(dynamodb, {
       TableName: users.tableName,
@@ -148,7 +192,7 @@ describe('examples', () => describe('users', () => {
       updatedAt: { S: currentDate.toISOString() },
     });
 
-    assert.deepStrictEqual(user2, {
+    assert.deepStrictEqual(entry2, {
       email: 'jdrydn@github.io',
       name: 'James',
       role: 'MODERATOR',
@@ -156,6 +200,105 @@ describe('examples', () => describe('users', () => {
       createdAt: currentDate,
       updatedAt: currentDate,
     });
+  });
+
+  it('should bulk-create some entries', async () => {
+    const entries = await users.bulkCreate([
+      {
+        email: 'jdrydn1@github.io',
+        name: 'James',
+        role: 'ADMIN',
+      },
+      {
+        email: 'jdrydn2@github.io',
+        name: 'James 2',
+        role: 'USER',
+      }
+    ]);
+
+    assert.ok(Array.isArray(entries), 'Expected users.bulkCreate to return an array');
+
+    await assertItem(dynamodb, {
+      TableName: users.tableName,
+      Key: { email: { S: 'jdrydn1@github.io' } },
+    }, {
+      email: { S: 'jdrydn1@github.io' },
+      name: { S: 'James' },
+      role: { S: 'ADMIN' },
+      createdAt: { S: currentDate.toISOString() },
+      updatedAt: { S: currentDate.toISOString() },
+    });
+    await assertItem(dynamodb, {
+      TableName: users.tableName,
+      Key: { email: { S: 'jdrydn2@github.io' } },
+    }, {
+      email: { S: 'jdrydn2@github.io' },
+      name: { S: 'James 2' },
+      role: { S: 'USER' },
+      createdAt: { S: currentDate.toISOString() },
+      updatedAt: { S: currentDate.toISOString() },
+    });
+
+    assert.deepStrictEqual(entries, [
+      {
+        email: 'jdrydn1@github.io',
+        name: 'James',
+        role: 'ADMIN',
+        createdAt: currentDate,
+        updatedAt: currentDate,
+      },
+      {
+        email: 'jdrydn2@github.io',
+        name: 'James 2',
+        role: 'USER',
+        createdAt: currentDate,
+        updatedAt: currentDate,
+      },
+    ]);
+  });
+
+  it('should bulk-get some entries', async () => {
+    const entries = await users.bulkGet([
+      { email: 'jdrydn1@github.io' },
+      { email: 'jdrydn2@github.io' }
+    ]);
+
+    assert.ok(Array.isArray(entries), 'Expected users.bulkGet to return an array');
+
+    assert.deepStrictEqual(entries, [
+      {
+        email: 'jdrydn1@github.io',
+        name: 'James',
+        role: 'ADMIN',
+        createdAt: currentDate,
+        updatedAt: currentDate,
+      },
+      {
+        email: 'jdrydn2@github.io',
+        name: 'James 2',
+        role: 'USER',
+        createdAt: currentDate,
+        updatedAt: currentDate,
+      },
+    ]);
+  });
+
+  it('should bulk-delete some entries', async () => {
+    const deleted = await users.bulkDelete([
+      { email: 'jdrydn1@github.io' },
+      { email: 'jdrydn2@github.io' }
+    ]);
+
+    assert.ok(deleted === true, 'Expected users.bulkDelete to return a boolean');
+
+    await assertItem(dynamodb, {
+      TableName: users.tableName,
+      Key: { email: { S: 'jdrydn1@github.io' } },
+    }, null);
+    await assertItem(dynamodb, {
+      TableName: users.tableName,
+      Key: { email: { S: 'jdrydn2@github.io' } },
+    }, null);
   });
 
 }));
