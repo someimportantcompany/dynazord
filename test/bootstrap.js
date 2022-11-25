65741abda74cb10820c167b2c2bd7ddf98363353
jdrydn <james@jdrydn.com>
2022-11-25 21:22:28 +0000

    Updated test to avoid use of super-global

--- test/bootstrap.js
+++ test/bootstrap.js
@@ -2,7 +2,3 @@
 const dotenv = require('dotenv');
 const path = require('path');
 dotenv.config({ path: path.resolve(__dirname, './.env') });
-
-const dynazord = require('dynazord');
-const { dynamodb } = require('./utils');
-dynazord.setDynamoDB(dynamodb);
